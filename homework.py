4cbb956edbb11775ea94b2c4d963f8d991611d8d
VulcanWong <wongvulcan@gmail.com>
2026-02-17 22:09:45 +0800

    lesson44

---
+++ homework.py
@@ -0,0 +1,11 @@
+my_subject = "python"
+print(my_subject)
+my_height = 1.45
+print(my_height)
+my_weight = 45.5
+print(my_weight)
+my_age = 10
+print(my_age)
+my_name = "vulcan"
+print(my_name)
+my_tuple = (my_subject, my_height, my_weight, my_age, my_name)
