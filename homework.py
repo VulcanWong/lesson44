b8f6097a6ddb1393fc3ac835666a9dcd21440baa
VulcanWong <wongvulcan@gmail.com>
2026-02-19 14:53:33 +0800

    lesson44

--- homework.py
+++ homework.py
@@ -1,11 +1,9 @@
-my_subject = "python"
-print(my_subject)
-my_height = 1.45
-print(my_height)
-my_weight = 45.5
-print(my_weight)
-my_age = 10
-print(my_age)
-my_name = "vulcan"
-print(my_name)
-my_tuple = (my_subject, my_height, my_weight, my_age, my_name)
+firstname = "vulcan"
+lastname = "wong"
+age = "10"
+height = "145"
+weight = "45"
+subject = "coding python"
+tuple = (firstname, lastname, age, height, weight, subject)
+print("this is my personal imformation")
+print(tuple)
